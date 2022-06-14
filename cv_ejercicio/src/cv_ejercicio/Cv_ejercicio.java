288fdb7c48c9a58a62c0ae6fee7a7b84921a5698
Usuario <Usuario@Usuario-PC>
2022-06-14 17:29:09 -0500

    version2

--- cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
+++ cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
@@ -17,6 +17,7 @@ public class Cv_ejercicio {
     public static void main(String[] args) {
        
         System.out.println("version 1");// TODO code application logic here
+        System.out.println("version2 ");
     }
     
 }
